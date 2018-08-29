a3b75eec950ce44ef9de7e15d6e1c088e027ba7f
Markus Hängsel <jag@markushangsel.com>
2018-08-29 23:45:11 +0200

    - lagt till sub-kommando för group  - hanterar on/off/brightness/colorTemp

--- index.js
+++ index.js
@@ -2,20 +2,66 @@
 const huejay = require('huejay');
 const program = require('commander');
 const config = require('config');
-let client;
+let client = new huejay.Client({
+  host:     config.bridge.host,        
+  username: config.bridge.username        
+}); 
 program
     .option('--bridges', 'List bridge devices')
-    .option('-r, --rooms [value]', 'List rooms')
+    .option('-, --groups [value]', 'List rooms')
     .option('--scenes', 'List scenes')
     .option('-v, --verbose', 'Show verbose info')
-    .option('-s, --set-scene [value]', 'Activate a scene by id')
-    .parse(process.argv);
+    .option('-s, --set-scene [value]', 'Activate a scene by id');
 
-    //console.log(program);
-    client = new huejay.Client({
-        host:     config.bridge.host,        
-        username: config.bridge.username        
-      });
+
+program
+  .command('group [value]')
+  .option('-o, --turn-on', 'turn on group')
+  .option('-x, --off', 'Turn off group')
+  .option('-b, --brightness [brightness]')
+  .option('-c, --color-temp [colorTemp]')
+  .action((value, cmd) => {    
+    if (value) {
+    client.groups.getById(value).then(group => {
+      // console.log(group);
+      // console.log(cmd);
+      if (cmd.brightness) {
+        group.brightness = parseInt(cmd.brightness);
+      }
+      if (cmd.colorTemp) {
+        group.colorTemp = parseInt(cmd.colorTemp);
+      }
+      if (cmd.off) {
+        group.on = false;
+        console.log("off");
+      }
+      if (cmd.turnOn) {
+        group.on = true;
+        console.log("on");
+      }
+      if (!(cmd.brightness || cmd.colorTemp || cmd.off || cmd.turnOn)) {
+        logGroup(group);
+      }
+
+      return client.groups.save(group);
+    });
+  } else {
+    client.groups.getAll()
+    .then(groups => {
+      for (let group of groups) {
+        logGroup(group);
+      }
+      });    
+  };
+});
+
+
+program.parse(process.argv);
+
+if (program.verbose) {
+  console.log(program);
+}
+
 
 
 if (program.bridges) {
@@ -70,8 +116,8 @@ if (program.rooms === true) { // List all rooms
         }
     }
   });
-} else if (program.rooms) {
-    client.groups.getById(program.rooms).then(group => {
+} else if (program.groups) {
+    client.groups.getById(program.groups).then(group => {
         console.log(`Group [${group.id}]: ${group.name}`);
         const allLights = group.lightIds.join(', ');
         console.log('  Light Ids: ' + allLights);
@@ -104,3 +150,11 @@ if (program.setScene) {
     console.log(error.stack);
   });
 }
+function logGroup(group) {
+  console.log(`Group [${group.id}]: ${group.name}`);
+  const allLights = group.lightIds.join(', ');
+  console.log('  Light Ids: ' + allLights);
+  console.log('  Brightness: ' + group.brightness);
+  console.log('  ColorTemp: ' + group.colorTemp);
+}
+
